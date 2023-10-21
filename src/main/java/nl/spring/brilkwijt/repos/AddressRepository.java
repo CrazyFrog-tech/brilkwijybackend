41e97a9e08349114c1368a213103f900e6d57b6b
allana <mohamad.allan@rigd-loxia.nl>
2023-10-21 16:41:07 +0200

    added color to backend

--- src/main/java/nl/spring/brilkwijt/repos/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-//package nl.spring.brilkwijt.repos;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import nl.spring.brilkwijt.repos.dto.Address;
-//
-//public interface AddressRepository extends JpaRepository<Address, Long> {
-//}
--- src/main/java/nl/spring/brilkwijt/repos/dto/Bril.java
+++ src/main/java/nl/spring/brilkwijt/repos/dto/Bril.java
@@ -1,17 +1,23 @@
 package nl.spring.brilkwijt.repos.dto;
 
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,6 +34,8 @@ public class Bril implements Serializable {
     private String description;
     @Column(name = "titel")
     private String titel;
+    @Column(name = "color")
+    private String color;
     @Column(name = "lostAt")
     private Date lostAtDate;
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
